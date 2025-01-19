afdfc68d8e3747c8013d60b1438c3c37e967873f
KyllianBeguin <KyllianBeguin@vivaldi.net>
2025-01-19 13:04:07 +0100

    [kb] ✨ Add file read and yaml deserilization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::fs;
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+use serde_yaml;
+use std::collections::BTreeMap;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+	/// The path to the config file to read
+    #[arg(default_value = "config.yml")]
+    path_config: std::path::PathBuf,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+	// The bamboo version
+	bamboo_version: i8,
+	// The dataset name
+	dataset_name: String,
+	// Columns configs
+	columns: BTreeMap<String, ColumnConfig>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ColumnConfig {
+	// Column name
+	_name: String,
+	// Column type
+	_type: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+	// Get arguments
+	let args = Cli::parse();
+
+	// Read file
+	let _content = fs::read_to_string(&args.path_config).expect("Unable to read file");
+	let _config: Config = serde_yaml::from_str(&_content).unwrap();
+	println!("Using bamboo version {} for dataset {}", _config.bamboo_version, _config.dataset_name);
+	for (_, col_config) in &_config.columns {
+		println!("{:?}", col_config)
+	}
 }
